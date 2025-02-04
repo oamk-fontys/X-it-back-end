de6a58f3e81d4d2c14a660e83bbee864266de1d6
Daan Verbeek <daanverbeek13@gmail.com>
2025-02-04 11:39:15 +0200

    Project upgrade & swagger installation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.enableCors();
+  app.setGlobalPrefix('api');
+
+  const config = new DocumentBuilder()
+    .setTitle('X-it API')
+    .setDescription('The X-it API description')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  const port = process.env.PORT ?? 3000;
+  Logger.log(`Server is running on port ${port}`, 'NestApplication');
+  await app.listen(port);
 }
 bootstrap();
