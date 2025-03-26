5b54fac5a88bedc74083f0ffcca1ee7bc4cad19f
suheda-snr <t3sesu00@students.oamk.fi>
2025-03-26 14:00:26 +0200

    add imports that is required after merge

--- src/modules/game/game.module.ts
+++ src/modules/game/game.module.ts
@@ -5,10 +5,14 @@ import { PrismaService } from '../../core/database/prisma.service';
 import { BookingService } from '../booking/booking.service';
 import { UserService } from '../user/user.service';
 import { RoomService } from '../room/room.service';
+import { TimeSlotModule } from "../time-slot/time-slot.module";
+import { UserModule } from "../user/user.module";
+import { RoomModule } from "../room/room.module";
 
 @Module({
   controllers: [GameController],
   providers: [GameService, PrismaService, BookingService, UserService, RoomService],
+  imports: [TimeSlotModule, UserModule, RoomModule],
   exports: [GameService],
 })
 export class GameModule { }
--- src/modules/player/player.module.ts
+++ src/modules/player/player.module.ts
@@ -7,10 +7,14 @@ import { GameService } from '../game/game.service';
 import { RoomService } from '../room/room.service';
 import { UserService } from '../user/user.service';
 import { BookingService } from '../booking/booking.service';
+import { TimeSlotModule } from "../time-slot/time-slot.module";
+import { UserModule } from "../user/user.module";
+import { RoomModule } from "../room/room.module";
 
 @Module({
   controllers: [PlayerController],
   providers: [PlayerService, PrismaService, GameService, UserService, RoomService, BookingService],
+  imports: [TimeSlotModule, UserModule, RoomModule],
   exports: [PlayerService],
 })
 export class PlayerModule { }
