fb6019a901eea83acdbd1f79a3e3920574adb46e
Daan Verbeek <daanverbeek13@gmail.com>
2025-03-28 12:21:52 +0100

    added page

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Res } from '@nestjs/common';
+import { Response } from 'express';
+import { join } from 'path';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +10,12 @@ export class AppController {
   @Get('/hello')
   getHello() {
     return this.appService.getHello();
+  }
+
+  @Get('file')
+  serveStatic(@Param('*') path: string, @Res() res: Response) {
+    console.log('serveStatic', path);
+    const filePath = join(__dirname, '..', 'public', path || 'index.html');
+    return res.sendFile(filePath);
   }
 }
