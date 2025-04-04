bab21b4f579e4eafe9204055d9b3d7ed938e44fe
XYYLG <547214@student.fontys.nl>
2025-04-04 11:13:39 +0200

    Updated the comment controller

--- src/modules/comment/comment.controller.ts
+++ src/modules/comment/comment.controller.ts
@@ -1,33 +1,71 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
-import { CreateEditCommentDto } from "./dto/create-edit-comment.dto";
-import { CommentService } from "./comment.service";
+import {
+    Body,
+    Controller,
+    Delete,
+    Get,
+    Param,
+    Post,
+    Put,
+    UseInterceptors,
+} from '@nestjs/common';
+import { ApiOkResponse } from '@nestjs/swagger';
+import { Role } from '@prisma/client';
+import { IsAuthenticated } from 'src/core/auth/auth.decorator';
+import { ResponseInterceptor } from 'src/core/interceptor/response.interceptor';
+import { CommentService } from './comment.service';
+import { CommentDto } from './dto/Comment.dto';
+import { CreateEditCommentDto } from './dto/create-edit-comment.dto';
 
 @Controller('comment')
 export class CommentController {
     constructor(private readonly commentService: CommentService) { }
 
-
     @Get()
+    @ApiOkResponse({
+        description: 'Get all comments',
+        type: CommentDto,
+        isArray: true,
+    })
+    @UseInterceptors(new ResponseInterceptor(CommentDto))
     async getComments() {
         return this.commentService.getComments();
     }
 
     @Get(':id')
+    @ApiOkResponse({
+        description: 'Get one comment by ID',
+        type: CommentDto,
+    })
     async getCommentById(@Param('id') id: string) {
         return this.commentService.getCommentById(id);
     }
 
     @Post()
-    async createComment(@Body() comment: CreateEditCommentDto) {
-        return this.commentService.createComment(comment);
+    @ApiOkResponse({
+        description: 'Create a comment',
+        type: CommentDto,
+    })
+    @IsAuthenticated([Role.USER, Role.ADMIN])
+    async createComment(@Body() body: CreateEditCommentDto) {
+        return this.commentService.createComment(body);
     }
 
     @Put(':id')
-    async updateComment(@Param('id') id: string, @Body() comment: CreateEditCommentDto) {
-        return this.commentService.updateComment(id, comment);
+    @ApiOkResponse({
+        description: 'Update a comment',
+        type: CommentDto,
+    })
+    @IsAuthenticated([Role.USER, Role.ADMIN])
+    async updateComment(@Param('id') id: string, @Body() body: CreateEditCommentDto) {
+        return this.commentService.updateComment(id, body);
     }
 
     @Delete(':id')
+    @ApiOkResponse({
+        description: 'Delete a comment',
+        type: CommentDto,
+    })
+    @IsAuthenticated([Role.ADMIN])
     async deleteComment(@Param('id') id: string) {
         return this.commentService.deleteComment(id);
     }
